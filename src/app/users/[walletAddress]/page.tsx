de5b92fb1851ef264b0ef644802e8bfbdf31d689
Jonathan Gan <jonathang132298@gmail.com>
2025-02-15 15:20:47 -0500

    punching air rn

--- src/app/users/[walletAddress]/page.tsx
+++ src/app/users/[walletAddress]/page.tsx
@@ -1,7 +1,12 @@
-import { useParams } from "next/navigation"
+type Props = {
+    params: Promise<{ walletAddress: string }>
+    searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}
 
-export default function UserPage() {
-    const { walletAddress } = useParams()
+export default async function UserPage(props: Props) {
+    const { params } = props
+    const { walletAddress } = await params
+    
     return (
         <div>
             <h1>User Page for {walletAddress}</h1>
